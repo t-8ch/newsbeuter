0b1ffd39ed430ab000932b423d9ff5e1e9890ea2
Thomas Weißschuh <thomas@t-8ch.de>
2013-04-29 19:20:02 +0000

    newsblur: only fetch unread articles

--- src/newsblur_api.cpp
+++ src/newsblur_api.cpp
@@ -119,7 +119,7 @@ rsspp::feed newsblur_api::fetch_feed(const std::string& id) {
 
 		std::string page = utils::to_string(i);
 
-		json_object * query_result = query_api("/reader/feed/" + id + "?page=" + page, NULL);
+		json_object * query_result = query_api("/reader/feed/" + id + "?read_filter=unread&page=" + page, NULL);
 
 		if (!query_result)
 			return f;
